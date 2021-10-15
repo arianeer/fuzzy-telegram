36f1322d1bd8f284451ecbadecc93b006c0d4bb5
ariane <aerob@kth.se>
2021-10-15 21:45:34 +0200

    setting things up

---
+++ src/themes.js
@@ -0,0 +1,9 @@
+export const theme = {
+    primaryDark: "#464C49",
+    primaryLight: "#899081",
+    primaryHover: "#464C49",
+    mobile: "576px",
+  
+    primaryTextDark: "#464C49",
+    primaryTextLight: "#CFC3AD",
+  };
